fb56a21434659f324c91cb4e2ab79147036039bf
nithin1819 <sainithin19@gmail.com>
2022-05-29 09:10:02 +0530

    Updates

---
+++ static/webpack/static/development/pages/campaigns/requests/new.js.031300f4af0cff49f0b8.hot-update.js
@@ -0,0 +1,19 @@
+webpackHotUpdate("static/development/pages/campaigns/requests/new.js",{
+
+/***/ 1:
+false,
+
+/***/ 6:
+/*!*******************************************************************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2Fcampaigns%2Frequests%2Fnew&absolutePagePath=%2Fcom.docker.devenvironments.code%2Fpages%2Fcampaigns%2Frequests%2Fnew.js&hotRouterUpdates=true ***!
+  \*******************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2Fcampaigns%2Frequests%2Fnew&absolutePagePath=%2Fcom.docker.devenvironments.code%2Fpages%2Fcampaigns%2Frequests%2Fnew.js&hotRouterUpdates=true! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fcampaigns%2Frequests%2Fnew&absolutePagePath=%2Fcom.docker.devenvironments.code%2Fpages%2Fcampaigns%2Frequests%2Fnew.js&hotRouterUpdates=true!./");
+
+
+/***/ })
+
+})
+//# sourceMappingURL=new.js.031300f4af0cff49f0b8.hot-update.js.map
